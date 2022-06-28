a60bb0765d89605cf4177a8997668e827f725bb3
Lord <lord.nemo@protonmail.com>
2022-06-28 20:36:20 +0300

    added elf generator

---
+++ elf-gen-test.cpp
@@ -0,0 +1,26 @@
+#include "elf-gen.h"
+
+static const uint8_t program[] = {
+    0x50,
+    0x4c, 0x8d, 0x14, 0x25, 0x04, 0x00, 0x00, 0x00,
+    0x48, 0xc7, 0xc0, 0x3c, 0x00, 0x00, 0x00, // mov rax, 60
+    0x48, 0xc7, 0xc7, 0x00, 0x00, 0x00, 0x00, // mov rdi, 0
+    0x0f, 0x05                                // syscall
+};
+
+
+int main()
+{
+    FILE *out = fopen("test.out", "w");
+    assert(out != NULL);
+
+    Elf64_Ehdr ehdr = elf_hdr_setup();
+    Elf64_Phdr phdr = progr_hdr_setup(sizeof(program));
+    fwrite(&ehdr, sizeof(ehdr), 1, out);
+    fwrite(&phdr, sizeof(phdr), 1, out);
+    const size_t offset_len = CODE_OFFSET_ - sizeof(ehdr) - sizeof(phdr);
+    uint8_t offset[offset_len] = {};
+    fwrite(offset,  sizeof(uint8_t), offset_len,  out);
+    fwrite(program, sizeof(uint8_t), sizeof(program), out);
+    fclose(out);
+}
